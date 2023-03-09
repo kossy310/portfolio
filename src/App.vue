654af5aec072a8c09ccd5ce6e8e9733b27a2b4ac
nakakoshi3100 <nakakoshi3100@gmail.com>
2023-03-09 18:40:23 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <section class="title">
+      <div class="titleContainer">
+        <h1>PORTFORIO</h1>
+        <h2>SOUICHI NAKAKOSHI</h2>
+      </div>
+    </section>
+    <section class="contents">
+      <nav>
+        <RouterLink to="/">WORK</RouterLink>
+        <RouterLink to="/about">DESIGN</RouterLink>
+        <RouterLink to="/movie">MOVIE</RouterLink>
+      </nav>
+      <RouterView />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+}
+
+.title {
+  width: 30%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+  font-size: 60px;
+  font-family: 'futura-pt', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+h2 {
+  font-family: 'linotype-didot', serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.contents {
+  width: 70%;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  width: 50%;
+  margin: 0px auto 0;
+}
+nav > a {
+  font-size: 20px;
+  font-family: 'futura-pt', sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/MovieView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="movie">
+    <h1>ムービーページ</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .movie {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
